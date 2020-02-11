5c6123dcda04752eb64a9a3502a49dd55ac3b3c8
Suraj Shrivastava <51397255+srjshrivastav@users.noreply.github.com>
2020-02-11 09:45:36 +0530

    Fix Bugs

--- Shortest_job_first.py
+++ Shortest_job_first.py
@@ -13,18 +13,18 @@ sorted_process=sorted(process.items(), key =
              lambda kv:(kv[1], kv[0])) 
 for i in sorted_process:
     print("|",i[0],end="")
-print("|\n%d"%(ct),end="   ")
+print(" |\n%d"%(ct),end="   ")
 for i in sorted_process:
     ct+=i[1]
-    tat[i[0]]=ct
     w_t[i[0]]=ct-i[1]
+    tat[i[0]]=ct-w_t[i[0]]
     t_w_t+=w_t[i[0]]
     print(ct,end="   ")
 table.field_names=["PID","Arriva Time","Burst Time","Waiting Time","Turn Around"]
 for process,bt in process.items():
     table.add_row([process,0,bt,w_t[process],tat[process]])
-print(table)
-print("ThroughPut :",n/ct)
+print("\n{0}".format(table))
+print("ThroughPut :",round(n/ct,3))
 print("Avg. waiting time :",round(t_w_t/n,2))
     
 
